ce86a8da0715b84e55fa0cf4a111ef074cc436b6
Klaus Andrew <klauzandrewnew@gmail.com>
2017-01-12 10:56:48 +0800

    Fixe sidebar link

--- resources/views/layouts/sidebar-place.blade.php
+++ resources/views/layouts/sidebar-place.blade.php
@@ -13,27 +13,27 @@
 			<div class="widget-body">
 				<div class="widget-main no-padding">
 					<ul class="county m-top-20">
-						<li><a href="page/Atlantic" data-state="AX">Atlantic</a></li>
-						<li><a href="page/Bergen" data-state="BN">Bergen</a></li>
-						<li><a href="page/Burlington" data-state="BU">Burlington</a></li>
-						<li><a href="page/Camden" data-state="CN">Camden</a></li>
-						<li><a href="page/Cape May" data-state="CM">Cape May</a></li>
-						<li><a href="page/Cumberland" data-state="CD">Cumberland</a></li>
-						<li><a href="page/Essex" data-state="EX">Essex</a></li>
-						<li><a href="page/Gloucester" data-state="GR">Gloucester</a></li>
-						<li><a href="page/Hudson" data-state="HN">Hudson</a></li>
-						<li><a href="page/Hunterdon" data-state="HU">Hunterdon</a></li>
-						<li><a href="page/Mercer" data-state="MR">Mercer</a></li>
-						<li><a href="page/Middlesex" data-state="MX">Middlesex</a></li>
-						<li><a href="page/Monmouth" data-state="MH">Monmouth</a></li>
-						<li><a href="page/Morris" data-state="MS">Morris</a></li>
-						<li><a href="page/Ocean" data-state="ON">Ocean</a></li>
-						<li><a href="page/Passaic" data-state="PX">Passaic</a></li>
-						<li><a href="page/Salem" data-state="SM">Salem</a></li>
-						<li><a href="page/Somerset" data-state="ST">Somerset</a></li>
-						<li><a href="page/SX" data-state="SX">Sussex</a></li>
-						<li><a href="page/Union" data-state="UN">Union</a></li>
-						<li><a href="page/Warren" data-state="WN">Warren</a></li>
+						<li><a href="{{ url('page/Atlantic') }}" data-state="AX">Atlantic</a></li>
+						<li><a href="{{ url('page/Bergen') }}" data-state="BN">Bergen</a></li>
+						<li><a href="{{ url('page/Burlington') }}" data-state="BU">Burlington</a></li>
+						<li><a href="{{ url('page/Camden') }}" data-state="CN">Camden</a></li>
+						<li><a href="{{ url('page/Cape May') }}" data-state="CM">Cape May</a></li>
+						<li><a href="{{ url('page/Cumberland') }}" data-state="CD">Cumberland</a></li>
+						<li><a href="{{ url('page/Essex') }}" data-state="EX">Essex</a></li>
+						<li><a href="{{ url('page/Gloucester') }}" data-state="GR">Gloucester</a></li>
+						<li><a href="{{ url('page/Hudson') }}" data-state="HN">Hudson</a></li>
+						<li><a href="{{ url('page/Hunterdon') }}" data-state="HU">Hunterdon</a></li>
+						<li><a href="{{ url('page/Mercer') }}" data-state="MR">Mercer</a></li>
+						<li><a href="{{ url('page/Middlesex') }}" data-state="MX">Middlesex</a></li>
+						<li><a href="{{ url('page/Monmouth') }}" data-state="MH">Monmouth</a></li>
+						<li><a href="{{ url('page/Morris') }}" data-state="MS">Morris</a></li>
+						<li><a href="{{ url('page/Ocean') }}" data-state="ON">Ocean</a></li>
+						<li><a href="{{ url('page/Passaic') }}" data-state="PX">Passaic</a></li>
+						<li><a href="{{ url('page/Salem') }}" data-state="SM">Salem</a></li>
+						<li><a href="{{ url('page/Somerset') }}" data-state="ST">Somerset</a></li>
+						<li><a href="{{ url('page/SX') }}" data-state="SX">Sussex</a></li>
+						<li><a href="{{ url('page/Union') }}" data-state="UN">Union</a></li>
+						<li><a href="{{ url('page/Warren') }}" data-state="WN">Warren</a></li>
 					</ul>
 				</div>
 			</div>
